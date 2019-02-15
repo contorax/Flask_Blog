35f0b1648d860306c86f92608303be288bf41ea2
Zafar Tagiyev <contorax@yandex.com>
2019-02-14 22:55:02 -0700

    Created Templates
    Templates - to reuse sections of code over multiple routes.

--- flaskblog.py
+++ flaskblog.py
@@ -1,16 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Walter White',
+        'title' : 'Blog Post 1',
+        'content' : 'First post content',
+        'date_posted' : 'September  10, 2018'
+    },
+    {
+        'author': 'Zafar Tagiyev',
+        'title' : 'Blog Post 2',
+        'content' : 'Second post content',
+        'date_posted' : 'February 9, 2019'
+
+    }
+    ]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 
 if __name__ == '__main__':
